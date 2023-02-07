12b8987a16ed873911bd88414cb461edfb25293e
VinicinBraga <vinicius@bragamatta.com.br>
2023-02-07 16:47:21 -0300

    setup themes - dark and light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/TopBar";
+import Sidebar from "./scenes/global/SideBar";
+import Dashboard from "./scenes/dashboard";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+
 function App() {
-  return <div className="app">App</div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
