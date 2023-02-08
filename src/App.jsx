f87cab64f9a3514af04f4441f36eac3ceffde7e2
VinicinBraga <vinicius@bragamatta.com.br>
2023-02-08 16:17:51 -0300

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
+import { useState } from "react";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/TopBar";
 import Dashboard from "./scenes/dashboard";
-import Sidebar from "./scenes/global/SideBar";
+import SideBar from "./scenes/global/SideBar";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <Sidebar />
+          <SideBar isSidebar={isSidebar} />
           <main className="content">
             <Topbar />
             <Routes>
