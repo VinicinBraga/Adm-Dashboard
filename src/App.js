e6444e2ae5ce12a6a49045a959dc9f1df347f8d0
VinicinBraga <vinicius@bragamatta.com.br>
2023-02-13 15:19:30 -0300

    Bar, Pie & line

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
 import FAQ from "./scenes/faq";
 import Calendar from "./scenes/calendar";
+import Bar from "./scenes/bar/index";
+import Pie from "./scenes/pie";
+import Line from "./scenes/line";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 
@@ -30,6 +33,9 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/calendar" element={<Calendar />} />
             </Routes>
